a81ef65102a33d930fd9692d477309d3febb1177
rahulgupta121 <rahulkumar3.1996@gmail.com>
2022-01-04 13:11:12 +0530

    up coming event section done

--- src/Components/UpComingEvent.js
+++ src/Components/UpComingEvent.js
@@ -1,119 +1,219 @@
-import React from 'react'
+import React from "react";
 
 const UpComingEvent = () => {
-    return (
-        <>
-         <div className='upcoming-event-background'>
-             <div className='container'>
-                 <div className='row '>
+  return (
+    <>
+      <div className="upcoming-event-background">
+        <div className="container">
+          <div className="row ">
+            <div className="col-sm-12 col-md-4 col-lg-4 mt-5">
+              <h5 className="text-white text-uppercase">
+                Up Coming Events Workshops, Roundtables, And Webinars
+              </h5>
+              <p className="text-white">
+                A much mammoth because sedulously that in more regarding
+                coaxingly wallaby more ouch fluently saw rabbit talkatively tore
+                less like about despite..
+              </p>
+              <div className="up-coming-event-btn-div">
+                <button
+                  type="button"
+                  className="btn btn-primary up-coming-event-btn"
+                >
+                  VIEW ALL EVENT
+                </button>
+              </div>
+            </div>
 
-                    <div className='col-sm-12 col-md-4 col-lg-4 mt-5'>
-                        <h5 className='text-white text-uppercase'>Up Coming Events Workshops, Roundtables, And Webinars</h5>
-                        <p className='text-white'>A much mammoth because sedulously that in more regarding coaxingly wallaby more ouch fluently saw rabbit talkatively tore less like about despite..</p>
-                        <div className='up-coming-event-btn-div'>
-                            <button type='button' className='btn btn-primary up-coming-event-btn'>VIEW ALL EVENT</button>
-                        </div>
-                   </div>
-  
-                    <div className='col-sm-12 col-md-8 col-lg-8 mt-5'>
-                        <div className='row'>
-                        <div className=' col-sm-12 col-md-12 col-lg-12 mb-3'>
-                           <div className='row upcoming-event-content-2'>
-                               <div className='event-date col-sm-12 col-md-1 col-lg-1'>
-                                    <h6 className='text-dark'><span className='nov27'>27</span> Nov</h6>
-                               </div>
-                               <div className='col-sm-12 col-md-8 col-lg-8'>
-                                   <h6 className='text-uppercase event-content-para-2 pt-2'>Activists for Gender Equality Financing</h6>
-                                   <ul className='ul-li-content-para-2 list-inline list-unstyled'>
-                                       <li className='li-content-para-2'><i class="fa fa-clock list-inline-item" aria-hidden="true"></i><span className='clock-text'> 06:00-05:30</span></li>
-                                       <li className='li-content-para-2'><i class="fa fa-globe list-inline-item" aria-hidden="true"></i><span className='location-text'>County Durham , UK</span></li>
-                                   </ul>
-                                   <div className='event-view-more-div mb-2'>
-                                       <button className='event-view-more btn btn-primary'>View Details</button>
-                                   </div>
-                               </div>
-                               <div className='col-sm-12 col-md-3 col-lg-3'>
-                                   <div>
-                                       <ul className='ul-li-content-para-3 list-inline list-unstyled'>
-                                           <li className='li-content-para-3 '><i class="fa fa-calendar calender-logo list-inline-item" aria-hidden="true"></i> Add</li>
-                                           <li className='li-content-para-3'><i class="fa fa-map-marker location-logo list-inline-item" aria-hidden="true"></i>Location</li>
-                                       </ul> 
-                                   </div>
-                               </div>
-                           </div>
-                            </div>
-                            <div className=' col-sm-12 col-md-12 col-lg-12  mb-3'>
-                            <div className='row'>
-                            <div className=' col-sm-12 col-md-12 col-lg-12'>
-                           <div className='row upcoming-event-content-2'>
-                               <div className='event-date col-sm-12 col-md-1 col-lg-1'>
-                                    <h6 className='text-dark'><span className='nov27'>15</span> Dec</h6>
-                               </div>
-                               <div className='col-sm-12 col-md-8 col-lg-8'>
-                                   <h6 className='text-dark text-uppercase event-content-para-2'>Activists for Gender Equality Financing</h6>
-                                   <ul className='ul-li-content-para-2 list-inline list-unstyled'>
-                                       <li className='li-content-para-2'><i class="fa fa-clock list-inline-item" aria-hidden="true"></i><span className='clock-text'>06:00-05:30</span> </li>
-                                       <li className='li-content-para-2'><i class="fa fa-globe list-inline-item" aria-hidden="true"></i><span className='location-text'> County Durham , UK</span></li>
-                                   </ul>
-                                   <div className='event-view-more-div mb-2'>
-                                       <button className='event-view-more btn btn-primary'>View Details</button>
-                                   </div>
-                               </div>
-                               <div className='col-sm-12 col-md-3 col-lg-3'>
-                               <div>
-                                       <ul className='ul-li-content-para-3 list-inline list-unstyled'>
-                                           <li className='li-content-para-3'><i class="fa fa-calendar calender-logo li-calender list-inline-item" aria-hidden="true"></i> Add</li>
-                                           <li className='li-content-para-3'><i class="fa fa-map-marker location-logo li-location list-inline-item" aria-hidden="true"></i>Location</li>
-                                       </ul> 
-                                   </div>
-                               </div>
-                           </div>
-                            </div>
-                            </div>
-                            </div>
-                            <div className=' col-sm-12 col-md-12 col-lg-12  mb-3'>
-                            <div className='row'>
-                            <div className='col-sm-12 col-md-12 col-lg-12'>
-                           <div className='row upcoming-event-content-2'>
-                               <div className='event-date col-sm-12 col-md-1 col-lg-1'>
-                                    <h6 className='text-dark'><span className='nov27'>05</span> Jan</h6>
-                               </div>
-                               <div className='col-sm-12 col-md-8 col-lg-8'>
-                                   <h6 className='text-dark text-uppercase event-content-para-2'>Activists for Gender Equality Financing</h6>
-                                   <ul className='ul-li-content-para-2 list-inline list-unstyled'>
-                                       <li className='li-content-para-2'><i class="fa fa-clock list-inline-item" aria-hidden="true"></i><span className='clock-text'>06:00-05:30</span></li>
-                                       <li className='li-content-para-2'><i class="fa fa-globe list-inline-item" aria-hidden="true"></i><span className='location-text'>County Durham , UK</span></li>
-                                   </ul>
-                                   <div className='event-view-more-div mb-2'>
-                                       <button className='event-view-more btn btn-primary'>View Details</button>
-                                   </div>
-                               </div>
-                               <div className='col-sm-12 col-md-3 col-lg-3'>
-                               <div>
-                                       <ul className='ul-li-content-para-3 list-inline list-unstyled'>
-                                           <li className='li-content-para-3'><i class="fa fa-calendar calender-logo list-inline-item" aria-hidden="true"></i> Add</li>
-                                           <li className='li-content-para-3'><i class="fa fa-map-marker location-logo list-inline-item" aria-hidden="true"></i>Location</li>
-                                       </ul> 
-                                   </div>
-                               </div>
-                           </div>
-                            </div>
-                            </div>
-                            </div>
-                        </div>
+            <div className="col-sm-12 col-md-8 col-lg-8 mt-5">
+              <div className="row">
+                <div className=" col-sm-12 col-md-12 col-lg-12 mb-3">
+                  <div className="row upcoming-event-content-2">
+                    <div className="event-date col-sm-12 col-md-1 col-lg-1">
+                      <h6 className="text-dark">
+                        <span className="nov27">27</span> Nov
+                      </h6>
                     </div>
-                 </div>
-             </div>
-             </div>   
-        </>
-    )
-}
+                    <div className="col-sm-12 col-md-8 col-lg-8">
+                      <h6 className="text-uppercase event-content-para-2 pt-2">
+                        Activists for Gender Equality Financing
+                      </h6>
+                      <ul className="ul-li-content-para-2 list-inline list-unstyled">
+                        <li className="li-content-para-2">
+                          <i
+                            class="fa fa-clock list-inline-item"
+                            aria-hidden="true"
+                          ></i>
+                          <span className="clock-text"> 06:00-05:30</span>
+                        </li>
+                        <li className="li-content-para-2">
+                          <i
+                            class="fa fa-globe list-inline-item"
+                            aria-hidden="true"
+                          ></i>
+                          <span className="location-text">
+                            County Durham , UK
+                          </span>
+                        </li>
+                      </ul>
+                      <div className="event-view-more-div mb-2">
+                        <button className="event-view-more btn btn-primary">
+                          View Details
+                        </button>
+                      </div>
+                    </div>
+                    <div className="col-sm-12 col-md-3 col-lg-3">
+                      <div>
+                        <ul className="ul-li-content-para-3 list-inline list-unstyled">
+                          <li className="li-content-para-3 ">
+                            <i
+                              class="fa fa-calendar calender-logo list-inline-item"
+                              aria-hidden="true"
+                            ></i>{" "}
+                            Add
+                          </li>
+                          <li className="li-content-para-3">
+                            <i
+                              class="fa fa-map-marker location-logo list-inline-item"
+                              aria-hidden="true"
+                            ></i>
+                            Location
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className=" col-sm-12 col-md-12 col-lg-12  mb-3">
+                  <div className="row">
+                    <div className=" col-sm-12 col-md-12 col-lg-12">
+                      <div className="row upcoming-event-content-2">
+                        <div className="event-date col-sm-12 col-md-1 col-lg-1">
+                          <h6 className="text-dark">
+                            <span className="nov27">15</span> Dec
+                          </h6>
+                        </div>
+                        <div className="col-sm-12 col-md-8 col-lg-8">
+                          <h6 className="text-dark text-uppercase event-content-para-2">
+                            Activists for Gender Equality Financing
+                          </h6>
+                          <ul className="ul-li-content-para-2 list-inline list-unstyled">
+                            <li className="li-content-para-2">
+                              <i
+                                class="fa fa-clock list-inline-item"
+                                aria-hidden="true"
+                              ></i>
+                              <span className="clock-text">06:00-05:30</span>{" "}
+                            </li>
+                            <li className="li-content-para-2">
+                              <i
+                                class="fa fa-globe list-inline-item"
+                                aria-hidden="true"
+                              ></i>
+                              <span className="location-text">
+                                {" "}
+                                County Durham , UK
+                              </span>
+                            </li>
+                          </ul>
+                          <div className="event-view-more-div mb-2">
+                            <button className="event-view-more btn btn-primary">
+                              View Details
+                            </button>
+                          </div>
+                        </div>
+                        <div className="col-sm-12 col-md-3 col-lg-3">
+                          <div>
+                            <ul className="ul-li-content-para-3 list-inline list-unstyled">
+                              <li className="li-content-para-3">
+                                <i
+                                  class="fa fa-calendar calender-logo li-calender list-inline-item"
+                                  aria-hidden="true"
+                                ></i>{" "}
+                                Add
+                              </li>
+                              <li className="li-content-para-3">
+                                <i
+                                  class="fa fa-map-marker location-logo li-location list-inline-item"
+                                  aria-hidden="true"
+                                ></i>
+                                Location
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className=" col-sm-12 col-md-12 col-lg-12  mb-3">
+                  <div className="row">
+                    <div className="col-sm-12 col-md-12 col-lg-12">
+                      <div className="row upcoming-event-content-2">
+                        <div className="event-date col-sm-12 col-md-1 col-lg-1">
+                          <h6 className="text-dark">
+                            <span className="nov27">05</span> Jan
+                          </h6>
+                        </div>
+                        <div className="col-sm-12 col-md-8 col-lg-8">
+                          <h6 className="text-dark text-uppercase event-content-para-2">
+                            Activists for Gender Equality Financing
+                          </h6>
+                          <ul className="ul-li-content-para-2 list-inline list-unstyled">
+                            <li className="li-content-para-2">
+                              <i
+                                class="fa fa-clock list-inline-item"
+                                aria-hidden="true"
+                              ></i>
+                              <span className="clock-text">06:00-05:30</span>
+                            </li>
+                            <li className="li-content-para-2">
+                              <i
+                                class="fa fa-globe list-inline-item"
+                                aria-hidden="true"
+                              ></i>
+                              <span className="location-text">
+                                County Durham , UK
+                              </span>
+                            </li>
+                          </ul>
+                          <div className="event-view-more-div mb-2">
+                            <button className="event-view-more btn btn-primary">
+                              View Details
+                            </button>
+                          </div>
+                        </div>
+                        <div className="col-sm-12 col-md-3 col-lg-3">
+                          <div>
+                            <ul className="ul-li-content-para-3 list-inline list-unstyled">
+                              <li className="li-content-para-3">
+                                <i
+                                  class="fa fa-calendar calender-logo list-inline-item"
+                                  aria-hidden="true"
+                                ></i>{" "}
+                                Add
+                              </li>
+                              <li className="li-content-para-3">
+                                <i
+                                  class="fa fa-map-marker location-logo list-inline-item"
+                                  aria-hidden="true"
+                                ></i>
+                                Location
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default UpComingEvent
-
-// {/* 
-//  <div className='row'>
-//                                 <div className='col-sm-12 col-md-2 col-lg-4'>
-
-//                                 </div>
-//                             </div> */}
+export default UpComingEvent;
